7706f41c045468eec3e00c467a590e224b50c216
Rohit Mishra <callrohit33@gmail.com>
2020-04-29 00:21:05 +0530

    Fixing Date Pickers and Formatting

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Dpr } from '../Dpr';
-import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+    selector: 'app-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  model: NgbDateStruct;
-  obj: Dpr = new Dpr();
-  db: Dpr[]=[];
+    model: NgbDateStruct;
+    obj: Dpr = new Dpr();
+    db: Dpr[] = [];
 
-  submitted = false;
+    submitted = false;
 
-  addToDB() {
-    this.submitted = true;
-    this.db.push(this.obj);
-    this.obj = new Dpr();
-  }
+    addToDB() {
+        this.submitted = true;
+        this.db.push(this.obj);
+        this.obj = new Dpr();
+    }
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
