3883071303c39e953117888ebd83979690bd2533
Rohit Mishra <callrohit33@gmail.com>
2020-04-28 23:55:21 +0530

    Fixing imports

--- src/app/Dpr.ts
+++ src/app/Dpr.ts
@@ -1,15 +1,15 @@
-import {Lf} from "./lf";
-import {Hec} from "./hec";
-import {Sample} from "./sample";
-import {Nbup} from "./nbup";
-import {Ceasure} from "./ceasure";
-import {TestData} from "./TestData";
+import { Lf } from './Lf';
+import { Hec } from './Hec';
+import { Sample } from './Sample';
+import { Nbup } from './Nbup';
+import { Ceasure } from './Ceasure';
+import { TestData } from './TestData';
 import { WorkOver } from './work-over';
 
 export class Dpr {
-    lf:Lf = new Lf();
-    hec:Hec = new Hec();
-    sample:Sample = new Sample();
+    lf: Lf = new Lf();
+    hec: Hec = new Hec();
+    sample: Sample = new Sample();
     nbup: Nbup = new Nbup();
     ceasure: Ceasure = new Ceasure();
     testData: TestData = new TestData();
