72a06d55267eb8303ceae8782c29921bf1b6b5c9
Rohit Mishra <callrohit33@gmail.com>
2020-04-29 22:50:17 +0530

    Adding particle js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dpr-dashboard';
+export class AppComponent implements OnInit {
+    title = 'dpr-dashboard';
+
+    myStyle: object = {};
+    myParams: object = {};
+    width = 100;
+    height = 100;
+
+    ngOnInit() {
+        this.myStyle = {
+            position: 'fixed',
+            width: '100%',
+            height: '100%',
+            'z-index': -1,
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+        };
+
+        this.myParams = {
+            particles: {
+                number: {
+                    value: 200,
+                },
+                color: {
+                    value: '#000000'
+                },
+                shape: {
+                    type: 'circle',
+                    stroke: {
+                        width: 0,
+                        color: '#000000'
+                    },
+                    polygon: {
+                        nb_sides: 5
+                    }
+                },
+                line_linked: {
+                    enable: true,
+                    distance: 100,
+                    color: '#000000',
+                    opacity: 0.4,
+                    width: 1
+                }
+            }
+        };
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule,enableProdMode } from '@angular/core';
+import { NgModule, enableProdMode } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgbDate, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ParticlesModule } from 'angular-particle';
 
 enableProdMode();
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent,
-  ],
-  imports: [
-    BrowserModule,
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FormComponent,
+    ],
+    imports: [
+        BrowserModule,
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        NgbModule,
+        ParticlesModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { 
+export class AppModule {
 }
